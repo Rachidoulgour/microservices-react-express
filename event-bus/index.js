635724b22186b27d7b3680cb92647c4b795d0609
Rachidoulgour <olgor@hotmail.com>
2024-05-08 11:01:47 +0200

    comment moderation and updating using moderation server

--- event-bus/index.js
+++ event-bus/index.js
@@ -8,9 +8,10 @@ app.use(cors());
 
 app.post('/events', (req, res) => {
     const event = req.body;
-    axios.post('http://localhost:5000/events', event)
-    axios.post('http://localhost:5001/events', event)
-    axios.post('http://localhost:5002/events', event)
+    axios.post('http://localhost:5000/events', event).catch((err) => { console.log(err.message); });
+    axios.post('http://localhost:5001/events', event).catch((err) => { console.log(err.message); });
+    axios.post('http://localhost:5002/events', event).catch((err) => { console.log(err.message); });
+    axios.post('http://localhost:5003/events', event).catch((err) => { console.log(err.message); });
     
     req.send({ status: 'OK' });
 });
