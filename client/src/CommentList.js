abd0a0908b5e9fb2ca02c8a2bc955bc21b74bb2f
Rachidoulgour <olgor@hotmail.com>
2024-05-08 09:03:13 +0200

    using query server in front

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -2,20 +2,10 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 
-const CommentList = (postId) => {
-    const [comments, setComments] = useState([]);
-    const fetchComments = async () => {
-        const res = await axios.get(`http://localhost:5001/posts/${postId}/comments`);
-        setComments(res.data);
-    }
-
-    useEffect(() => {
-        fetchComments();
-    }, []);
-
+const CommentList = ({comments}) => {
     const renderedComments = comments.map(comment => {
         return (
-            <li>{comment.content}</li>
+            <li key={comment.id}>{comment.content}</li>
         )
     })
 
