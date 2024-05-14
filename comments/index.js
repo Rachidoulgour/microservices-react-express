9dbc2c74166d0ed6d79476da6d17c305a9f63e2a
Rachidoulgour <olgor@hotmail.com>
2024-05-14 09:40:22 +0200

    urls modified to get connection between query, comments, moderation with event-bus pods

--- comments/index.js
+++ comments/index.js
@@ -20,7 +20,7 @@ app.post('/posts/:id/comments', async (req, res) => {
     comments.push({ id: commentId, content, status: 'pending' });
     commentsByPostId[req.params.id] = comments;
 
-    await axios.post('http://localhost:5005/events', {
+    await axios.post('http://event-bus-srv:5005/events', {
         type: 'CommentCreated',
         data: {
             id : commentId,
@@ -42,7 +42,7 @@ app.post('/events', async (req, res) => {
             return comment.id === id;
         });
         comment.status = status;
-        await axios.post('http://localhost:5005/events', {
+        await axios.post('http://event-bus-srv:5005/events', {
             type: 'CommentUpdated',
             data: {
                 id,
--- moderation/index.js
+++ moderation/index.js
@@ -9,7 +9,7 @@ app.post('events', async (req, res) => {
 
     if(type === 'CommentCreated') {
         const status = data.content.include('stupid') ? 'rejected' : 'approved';
-        await axios.post('http://localhost:5005/events', {
+        await axios.post('http://event-bus-srv:5005/events', {
             type: 'CommentModerated',
             data: {
                 id: data.id,
